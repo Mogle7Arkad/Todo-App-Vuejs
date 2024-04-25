9255de608171740cf6a11282498d3b2fc9cccf2b
Ali George <george7ali@gmail.com>
2024-04-25 23:13:04 +0100

    import the header component to the app component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="container">
-
+        <Header />
     </div>
 </template>
 
 <script>
+    import Header from './components/Header.vue'
     export default {
         name: 'App',
+        components: {
+            Header
+        },
     }
 </script>
 
