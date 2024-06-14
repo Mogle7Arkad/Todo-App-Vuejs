0771807b0b257bd0679bda4b2fefec5efefdd6b6
Ali George <george7ali@gmail.com>
2024-06-14 16:49:50 +0100

    import the tasks component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="container">
         <Header title="Todo App"/>
+        <Tasks :tasks="tasks" />
     </div>
 </template>
 
 <script>
     import Header from './components/Header.vue'
+    import Tasks from './components/Tasks.vue'
     export default {
       name: 'App',
       components: {
-          Header
+          Header,
+          Tasks
       },
       data() {
         return {
