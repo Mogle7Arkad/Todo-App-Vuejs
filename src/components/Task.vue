bdff89af974e83e845b886af65cdfdefb4edd129
Ali George <george7ali@gmail.com>
2024-06-15 08:24:16 +0100

    add class and apply scoped styling to the task component

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -8,5 +8,27 @@ export default {
 </script>
 
 <template>
+<div class="task">
     <h5>{{ task.text }}</h5>
+    <p>{{ task.day }}</p>
+</div>
 </template>
+
+<style scoped>
+.task{
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.task.reminder {
+    border-left: 5px solid green;
+}
+
+.task h5 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
